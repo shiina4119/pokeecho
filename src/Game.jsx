11a820527d0303befea9377f5f73be0c8b6d6115
shiina <152995083+shiina4119@users.noreply.github.com>
2024-03-13 01:59:05 +0530

    game logic works

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
 import PokeCard from "./PokeCard";
-import { Grid, CircularProgress, styled, Paper } from "@mui/material";
 import "./Game.css";
+import { createRandomArray } from "./utilities";
 
-export default function Game({ size }) {  
-    const [ pokemonList, setPokemonList ] = useState([]);
+export default function Game({ size }) {
+    const [ idList, setIdList ] = useState(createRandomArray(size));
+    
+    const [ allPokemonData, setAllPokemonData ] = useState([]);
     const [ error, setError ] = useState(null);
     const [ loading, setLoading ] = useState(true);
-    
-    const idList = Array.from({
-	length: size
-    }, () => Math.floor(Math.random() * 600) + 1);
+
+    const [ clickedIdList, setClickedIdList] = useState([]);
+    const [ gameOver, setGameOver ] = useState(false);
+    const [ score, setScore ] = useState(0);
+    const [ highScore, setHighScore ] = useState(0);
     
     useEffect(() => {
-	console.log("promise effect running");
 	const promises = idList.map(id =>
 	    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`));
 	
@@ -21,28 +23,62 @@ export default function Game({ size }) {
 	    .then(responses =>
 		Promise.all(responses.map(res =>
 		    res.json())))
-	    .then(data => { setPokemonList(data) })
-	    .catch(err => { setError(err) })
-	    .finally(() => { setLoading(false) });
-    }, []);
+	    .then(data => { setAllPokemonData(data); })
+	    .catch(err => {
+		setError(err);
+		console.log(error);
+	    })
+	    .finally(() => { setLoading(false); });
+    }, [idList]);
 
-    function handleClick() {
-	console.log("clicked");
-	setPokemonList((prevPokemonList) => [...prevPokemonList].sort(() => Math.random() - 0.5));
+    function handleCardClick(id) {
+	if (!gameOver) {
+	    if (clickedIdList.includes(id)) {
+		console.log("game over");
+		setGameOver(true);
+		if (score > highScore) {
+		    setHighScore(score);
+		}
+		setScore(0);
+		setClickedIdList([]);
+	    } else {
+		setClickedIdList([...clickedIdList, id]);
+		setScore(score + 1);
+		setAllPokemonData((prevPokemonData) => [...prevPokemonData].sort(() => Math.random() - 0.5));
+	    }
+	} else {
+	    console.log("try harder");
+	}
     };
 
+    function handleNewGameButton() {
+	setScore(0);
+	setGameOver(false);
+	setIdList(createRandomArray(size));
+	setClickedIdList([]);
+    }
+
     if(loading) {
-	return <CircularProgress />;
+	return <h1>loading</h1>;
     }
     else {
 	return (
 	    <>
+		<div>
+		    <p>Score: {score}</p>
+		    <p>High Score: {highScore}</p>
+		</div>
 		<div className="container">
-		    {pokemonList.map(pokemonData =>
-			<button key={pokemonData.id} className="card-button" onClick={() => { handleClick() }}>
+		    {allPokemonData.map(pokemonData =>
+			<button key={pokemonData.id} className="card-button" onClick={() => { handleCardClick(pokemonData.id) }}>
 			    <PokeCard data={pokemonData} />
 			</button>
 		    )}
+		</div>
+		<div>
+		    <p>
+			<button onClick={() => handleNewGameButton()}>New Game</button>
+		    </p>
 		</div>
 	    </>
 	);
---
+++ src/utilities.js
@@ -0,0 +1,8 @@
+export function createRandomArray(size) {
+    let arr = []
+    while (arr.length < size) {
+	let x = Math.floor(Math.random() * 600) + 1
+	if (arr.indexOf(x) === -1) arr.push(x)
+    }
+    return arr;
+}
