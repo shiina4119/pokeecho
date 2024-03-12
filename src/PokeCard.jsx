ab7a66afd5a41331021bc61863a23d5ceacc80c7
shiina <152995083+shiina4119@users.noreply.github.com>
2024-03-13 01:59:05 +0530

    game works need to add eyecandy

--- src/PokeCard.jsx
+++ src/PokeCard.jsx
@@ -3,7 +3,7 @@ import "./PokeCard.css";
 export default function PokeCard({ data }) {
     return (
 	<div className="card">
-	    <img src={data.sprites.other.dream_world.front_default} alt={data.name} />
+	    <img src={data.sprites.front_default} alt={data.name} />
 	    <div className="card-details">
 		{data.name}
 	    </div>
