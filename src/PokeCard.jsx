e3342b65c8a7164523e03c36d2d668b68cd83a2a
shiina <152995083+shiina4119@users.noreply.github.com>
2024-03-13 18:03:23 +0530

    cleaning

--- src/PokeCard.jsx
+++ src/PokeCard.jsx
@@ -4,7 +4,7 @@ export default function PokeCard({ data }) {
     return (
 	<div className="card">
 	    <img src={data.sprites.front_default} alt={data.name} />
-	    <div className="card-details typography">{data.name}</div>
+	    <div className="typography">{data.name}</div>
 	</div>
     );
 }
